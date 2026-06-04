e9ccecd2a4f4510383118abdc4ad960852e24083
SebastianAlecio <sebastianalecio@gmail.com>
2026-06-03 18:44:43 -0600

    Trivial change to trigger PR checks

--- infra/main.tf
+++ infra/main.tf
@@ -2,3 +2,5 @@ resource "aws_sqs_queue" "main" {
   name                       = var.queue_name
   visibility_timeout_seconds = var.visibility_timeout_seconds
 }
+
+# Pipeline evidence: ejercicio 7.2 — comentario trivial
